3cfff3721f3b7de0091fd1cbc4b28dd3c8a6ccfb
Patricio Pérez Pinto <patodeath@gmail.com>
2024-12-17 17:04:50 -0300

    fix: fix size when resize

--- src/js/app-resizer-listener.js
+++ src/js/app-resizer-listener.js
@@ -1,4 +1,3 @@
-// Ajustar el scroll al cargar y al redimensionar
 adjustAppListHeight();
 
 window.addEventListener('resize', adjustAppListHeight);
@@ -8,12 +7,7 @@ function adjustAppListHeight() {
     const searchBar = document.getElementById('searchBar');
     const appList = document.getElementById('app-list');
     const totalOffset = searchBar.offsetHeight + searchBar.offsetTop; // Altura + espacio desde arriba
-
-    // Resta cualquier margen o padding del contenedor padre
     const parentPaddingBottom = parseInt(window.getComputedStyle(appList.parentElement).paddingBottom) || 0;
-
-    // Calcular la altura restante de la ventana
     const availableHeight = window.innerHeight - totalOffset - parentPaddingBottom;
-
-    appList.style.height = `${availableHeight - 30}px`;
+    appList.style.height = `${availableHeight - 40}px`;
 }
--- src/js/search-bar-listener.js
+++ src/js/search-bar-listener.js
@@ -4,23 +4,28 @@ async function searchListener(e) {
     }
 
     const input = e.target;
-    const query = input.value.trim(); // Obtener el valor del input
+    const query = input.value.trim();
     const spinner = document.getElementById('spinner');
 
-    if (query) {
-        try {
-            spinner.style.display = 'block'; // Mostrar spinner
-            const response = await fetch(`http://localhost:8080/api/v1/packages/${query}`);
-            const data = await response.json();
+    if (!query) {
+        return;
+    }
 
-            // Filtrar y renderizar los resultados
-            const packages = data.packages || [];
-            renderAppList(packages);
-        } catch (error) {
-            log.info('Error fetching packages:', error);
-            renderAppList([]); // Limpiar lista si hay error
-        } finally {
-            spinner.style.display = 'none'; // Ocultar spinner
-        }
+    try {
+        const endpoint = `http://localhost:8080/api/v1/packages/${query}`;
+
+        log.info(endpoint);
+
+        spinner.style.display = 'block';
+        const response = await fetch(endpoint);
+        const data = await response.json();
+
+        const packages = data.packages || [];
+        renderAppList(packages);
+    } catch (error) {
+        log.info('Error fetching packages:', error);
+        renderAppList([]);
+    } finally {
+        spinner.style.display = 'none';
     }
 }
