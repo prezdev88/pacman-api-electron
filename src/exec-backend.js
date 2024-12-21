c297cc31ea30df840f5cdef17a71819fdaf4ddc9
Patricio Pérez Pinto <patodeath@gmail.com>
2024-12-21 02:19:49 -0300

    feat: add build app image option (with pacman api backend binary)

--- src/exec-backend.js
+++ src/exec-backend.js
@@ -6,13 +6,20 @@ const fs = require('fs');
 let apiProcess = null;
 
 function executeBin() {
-    const binaryPath = path.join(__dirname, 'bin', 'pacman-api');
+    const isDev = process.env.NODE_ENV === 'development' || process.defaultApp;
+    const basePath = isDev
+        ? path.join(__dirname, 'bin') // Ruta en desarrollo
+        : path.join(process.resourcesPath, 'bin'); // Ruta en producción
 
+    const binaryPath = path.join(basePath, 'pacman-api');
+
+    // Validar si el binario existe
     if (!fs.existsSync(binaryPath)) {
         log.error('El binario no existe en la ruta:', binaryPath);
         return;
     }
 
+    // Ejecutar el binario
     apiProcess = execFile(binaryPath, [], { cwd: path.dirname(binaryPath) }, (error, stdout, stderr) => {
         if (error) {
             log.error('Error ejecutando el binario:', error);
